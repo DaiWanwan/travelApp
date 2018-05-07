dba0b7292cf51b8a571b59f5adab2495bc0dcecc
DaiWanwan <m201672381@hust.edu.cn>
2018-05-07 18:59:39 +0800

    add cityheader

---
+++ src/pages/city/City.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <city-header></city-header>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  import CityHeader from './components/header.vue'
+  export default {
+    name: 'City',
+    components: {
+      CityHeader
+    }
+  }
+</script>
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+</style>
---
+++ src/pages/city/components/header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    name: 'CityHeader'
+  }
+</script>
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  @import "~styles/varibles.styl"
+  .header
+    position: relative
+    overflow: hidden
+    height: $headerHeight
+    line-height: $headerHeight
+    text-align: center
+    font-size: .32rem
+    color: #ffffff
+    background: $bgColor
+    .header-back
+      position: absolute
+      top: 0
+      left: 0
+      width: .64rem
+      text-align center
+      font-size: .4rem
+      color: #ffffff
+</style>
