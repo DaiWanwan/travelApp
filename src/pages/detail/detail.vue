5f1dcb30c6ede7e0573e7c521c65af4afbece650
DaiWanwan <m201672381@hust.edu.cn>
2018-05-10 16:18:48 +0800

    add list

--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -2,22 +2,71 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="List"></detail-list>
+    </div>
   </div>
 </template>
 <script type="text/ecmascript-6">
   import DetailBanner from './components/banner.vue'
   import DetailHeader from './components/header.vue'
+  import DetailList from './components/list.vue'
   export default {
     name: 'Detail',
     components: {
       DetailBanner,
-      DetailHeader
+      DetailHeader,
+      DetailList
+    },
+    data () {
+      return {
+        List: [
+          {
+            title: '成人票',
+            children: [
+              {
+                title: '成人三馆联票'
+              }, {
+                title: '成人五馆联票'
+              }, {
+                title: '成人全馆联票'
+              }
+            ]
+          },
+          {
+            title: '学生票',
+            children: [
+              {
+                title: '成人三馆联票',
+                children: [{title: 'xxxx销售'}]
+              }, {
+                title: '成人五馆联票'
+              }, {
+                title: '成人全馆联票'
+              }
+            ]
+          },
+          {
+            title: '儿童票'
+          },
+          {
+            title: '特惠票',
+            children: [
+              {
+                title: '成人三馆联票'
+              }, {
+                title: '成人五馆联票'
+              }, {
+                title: '成人全馆联票'
+              }
+            ]
+          }
+        ]
+      }
     }
   }
 </script>
 <style lang="stylus" rel="stylesheet/stylus" scoped>
   .content
     height: 50rem
-    background: #e5c5ff
 </style>
